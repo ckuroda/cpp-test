35cca4eda80772510e75edb3ff357e1a114c4d3a
ckuroda <claudio.r.kuroda@gmail.com>
2016-12-07 15:41:22 -0200

    esqueleto

--- challenges/classroom/classroom.cpp
+++ challenges/classroom/classroom.cpp
@@ -9,9 +9,16 @@ int main(int argc, char** argv)
 {
 	// declaracao de variaveis
 	FILE *fp;
-	char str[80];
+	char str[80],
+	     str1[40],
+             str2[40];
+	char *posSpace;
 	int  qtQuestion = 0,
-	     nrQuestion = 0;
+	     nrQuestion = 0,
+	     nrTamStr = 0,
+	     nrTamStr1 = 0,
+	     nrTamStr2 = 0,
+	     idOk = 1;
 	
 	if (argc == 2) {
 		// parametros ok
@@ -21,11 +28,36 @@ int main(int argc, char** argv)
 			try {
 				qtQuestion = atoi(str);
 			} catch (int e) {
+				cout << "Erro na linha 1: qtde de questionamentos. Exception nr. " << e << '\n';
+				return -1;
 			}
-			while (!feof(fp) && (nrQuestion < qtQuestion)) {
+			while (!feof(fp) && (nrQuestion < qtQuestion) && idOk) {
 				// novo questionamento
+				fgets(str);
+				posSpace = strchr(str,' ');
+				nrTamStr = strlen(str);
+				nrTamStr2 = strlen(posSpace) - 1;
+				nrTamStr1 = nrTamStr - nrTamStr2 - 1;
+				if ((nrTamStr1 > 0) && (nrTamStr2 > 0)) {
+					// dois valores ok
+					try {
+						idQuestion = atoi(nrStr1);
+						idQuestionQtde = atoi(nrStr2);
+					} catch (int e) {
+						cout << "Linha de questionamento invalido. Exception nr. " << e << '\n';
+					}
+				} else {
+					// questionamento invalido
+					printf("Questionamento invalido.");
+					idOk = 0;
+				}
 			}
-			return 0;
+			// valida questionamentos invalidos
+			if (idOk) { 
+				return 0;
+			} else {
+				return -1;
+			}
 		} else {
 			// falha em abertura de arquivo
 			return -1;
