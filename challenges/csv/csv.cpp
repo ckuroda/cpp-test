5aa4474b76775163673c5ea95a5efd9fdffc0b05
ckuroda <claudio.r.kuroda@gmail.com>
2016-12-07 18:04:29 -0200

    grava registros

--- challenges/csv/csv.cpp
+++ challenges/csv/csv.cpp
@@ -6,5 +6,63 @@
 
 int main(int argc, char** argv)
 {
-	return 0;
+	// declaracao de variaveis
+	FILE *fp,
+	     *fout;
+	char str[255],
+	     nextStr[255],
+	     fields[100][10][255],
+	     sOut[255];
+	int  idField,
+	     nrTamStr,
+	     nrTamNextStr,
+	     nrLine;
+	
+	if (argc != 3) {
+		// parametros invalidos
+		printf("Erro de parametros.");
+		return -1;
+	} else {
+		// parametros ok
+		if ((fp=fopen(argv[1],"r"))==NULL || (fout=fopen(argv[2],"w"))==NULL) {
+			printf("Erro na abertura de arquivos.");
+			return -1;
+		} else {
+			// abre arquivo ok - le novo registro
+			fgets(str,255,fp);
+			try {
+				qtLines = atoi(str);
+			} catch (int e) {
+				cout << "Linha 1 invalida. Exception nr. " << e << '\n';
+				return -1;
+			}
+			nrLine=0;
+			// le registro a registro
+			while (!feof(fp) && (nrLine < qtLines)) {
+				fgets(str,255,fp);
+				idField=0;
+				// grava campos do registro em fields
+				do {
+					nextStr = strchr(str,',');
+					if (nextStr == NULL) {
+						fields[nrLine][idField] = str;
+					} else {
+						nrTamStr = strlen(str);
+						nrTamNextStr = strlen(nextStr);
+						strncpy(fields[nrLine][idField],str,nrTamStr-nrTamNextStr);
+						str=nextStr+1;
+						idField++;
+					}
+				} while (nextStr != NULL);
+				nrLine++;
+			}		
+			// gera saida registro a registro
+			for (i=0;i<=idField;i++) {
+			}
+			fclose(fp);
+			fclose(fout);
+			return 0;
+		}
+		
+	}
 }
