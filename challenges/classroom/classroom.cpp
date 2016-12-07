92eb6ce1a10092dd2609649a7da8f5c5a9a3d297
ckuroda <claudio.r.kuroda@gmail.com>
2016-12-07 14:59:25 -0200

    v1

--- challenges/classroom/classroom.cpp
+++ challenges/classroom/classroom.cpp
@@ -7,5 +7,32 @@
 
 int main(int argc, char** argv)
 {
-	return 0;
+	// declaracao de variaveis
+	FILE *fp;
+	char str[80];
+	int  qtQuestion = 0,
+	     nrQuestion = 0;
+	
+	if (argc == 2) {
+		// parametros ok
+		if ((fp=fopen(argv[1],"r")) == NULL) {
+			// abre arquivo ok
+			fgets(str);
+			try {
+				qtQuestion = atoi(str);
+			} catch (int e) {
+			}
+			while (!feof(fp) && (nrQuestion < qtQuestion)) {
+				// novo questionamento
+			}
+			return 0;
+		} else {
+			// falha em abertura de arquivo
+			return -1;
+		}
+	} else {
+		// parametros invalidos
+		printf("Parametros invalidos.");
+		return -1;
+	}
 }
