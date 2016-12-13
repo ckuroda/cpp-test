038e8dc735754619a9920a701e8d193a5ebe710b
ckuroda <claudio.r.kuroda@gmail.com>
2016-12-13 12:11:53 -0200

    test ok

--- challenges/fibonacci/fibonacci.cpp
+++ challenges/fibonacci/fibonacci.cpp
@@ -2,61 +2,31 @@
 
 int main(int argc, char** argv)
 {
-	// declara variaveis
-	FILE *fp,
-	     *fout;
-	char str[80];
-	int  nr1=0,
-	     nr2=1,
-	     nrSwap,
-	     nrSoma;
-	
-	if (argc != 3) {
-		// argumentos invalidos
-		cout << "Argumentos invalidos.";
-		return -1;
-	} else {
-		// qtde arq ok
-		if ((fp=fopen(argv[1],"r"))==NULL || (fout=fopen(argv[2],"w"))==NULL) {
-			// abre arq erro
-			cout << "Erro na abertura de arquivos.";
-			return -1;
-		} else {
-			// abre arq ok
-			try {
-				fgets(str,80,fp);
-			} catch (int e) {
-				cout << "Erro no indice fibonacci. Exception nr. " << e << '\n';
-				return -1;
-			}
-			try {
-				nrIndice = atoi(str);
-			} catch (int e) {
-				cout << "Indice invalido. Exception nr. " << e << '\n';
-				return -1;
-			}
-			switch (nrIndice) {
-				case 1: nrSoma = 0;
-					break;
-				case 2: nrSoma = 1;
-					break;
-				default:
-					// calcula fibonacci
-					for (i=1;i<nrIndice;i++) {
-						nrSoma = nr1+nr2;
-						nr1 = nr2;
-						nr2 = nrSoma;
-					}
-			}
-			// gera saida
-			try {
-				itoa(nrSoma,nrTmp,10);
-				
-				fputs(nrTmp,fout);
-			} catch (int e) {
-				fputs("Erro no calculo.",fout);
-			}
-			return 0;
-		}
-	}
+	//vars
+	int iQuestionNr=0,
+	    iNum1=0,
+	    iNum2=1,
+	    iResult=0,
+	    iIndex=1;
+
+	// row 1
+	std::cin >> iQuestionNr;
+
+	// fibonacci compute
+	while (iIndex < iQuestionNr) {
+
+		// sum new value
+		iResult = iNum1 + iNum2;
+
+		// move values
+		iNum1 = iNum2;
+		iNum2 = iResult;	
+
+		// next value
+		iIndex++;
+
+	} // while	
+
+	// set out
+	std::cout << iResult << std::endl;
 }
