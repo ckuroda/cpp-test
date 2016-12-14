aa0c166a33a9656f37b8dd1d3893f2fd976263e2
ckuroda <claudio.r.kuroda@gmail.com>
2016-12-14 14:20:32 -0200

    test ok

--- challenges/palindrome/palindrome.cpp
+++ challenges/palindrome/palindrome.cpp
@@ -1,65 +1,51 @@
 #include <iostream>
 #include <string>
 
-int isPalindrome (char *s);
-char *setMessage (char *s);
+using namespace std;
+
+int isPalindrome (string s);
 
 int main(int argc, char** argv)
 {
-	// variaveis
-	FILE *fp,
-	     *fout;
-	char str[80];
-	int  nrLinhas,
-	     idLinha=0;
-	
-	if (argc != 3) {
-		// argumentos invalidos
-		cout << "Argumentos invalidos.";
-		return -1;
-	} else {
-		if ((fp=fopen(argv[1],"r"))==NULL || (fout=fopen(argv[2],"w"))==NULL) {
-			// abre arq erro
-			cout << "Erro na abertura de arquivos.";
-			return -1;
+	string sWord,
+	       sMessage;
+
+	int  iRowCount,
+	     iActualRow=0;
+
+	// row 1 - row count	
+	std::cin >> iRowCount;
+
+	// file scan
+	while (iActualRow < iRowCount) {
+
+		std::cin >> sWord;
+
+		if (isPalindrome(sWord)) {
+			sMessage = " is palindrome";
 		} else {
-			fgets(str,80,fp);
-			try {
-				nrLinhas = atoi(str);
-			} catch (int e) {
-				cout << "Erro na linha 1. Exception nr. " << e << '\n';
-				return -1;
-			}
-			// varre arquivo
-			while ((!feof(fp)) && (idLinha < nrLinhas)) {
-				fgets(str,80,fp);
-				if (isPalindrome(str)) {
-					fputs(setMessage(str),fout);
-				}
-				idLinha++;
-			}
-			return 0;
-			fclose(fp);
-			fclose(fout);
+			sMessage = " is not palindrome";
 		}
+
+		// set out
+		std::cout << sWord << sMessage << std::endl;
+
+		iActualRow++;
+
+	} // while file scan
+
+	return 0;
+
 }
 
-int isPalindrome (char *s) {
-	char sAux[80];
-	int  nrTamS=0;
+int isPalindrome (string s) {
+	string sAux="";
 	
-	nrTamS = strlen(s);
 	//sAux recebe inverso de s
-	for (i=nrTamS-1;i>=0;i--) {
-		strcat(sAux,s[i]);
+	for (size_t i=s.length();i>0;i--) {
+		sAux += s[i-1];
 	}
-	return (!strcmp(s,sAux));
-}
-	
-char *setMessage (char *s) {
-	char sAux[80];
-	
-	strcat(sAux,s);
-	strcat(sAux," is palindrome");
-	return sAux;
+
+	return (s == sAux);
+
 }
